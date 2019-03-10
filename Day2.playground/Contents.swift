45ccd52826ceb2a9e51732d2c84d3472caa37d24
admin <admin@ACE20.local>
2019-03-10 11:11:33 +0530

    Bored

--- Day2.playground/Contents.swift
+++ Day2.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var str = "Hello, playground"
+//Emojis
+
+var heart = "💗"
+print(heart)
+
+var 🐱 = "CAT"
+print (🐱)
+
+var emotion_dict = [String:String]()
+emotion_dict = ["Happy":"😁","Sad":"☹️","Angry":"😡"]
+func check_emotion (howareyoufeeling emotion : String)
+{
+    for i in emotion_dict
+    {
+        if(i.key==emotion)
+        {
+            print("The emotion is :" , i.value)
+        }
+    }
+}
+check_emotion(howareyoufeeling: "Angry")
