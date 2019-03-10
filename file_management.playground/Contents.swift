6b2e8a297e9b7ce584ebc1c8ea723fdae97f2322
admin <admin@ACE20.local>
2019-03-10 14:34:50 +0530

    Updates

--- file_management.playground/Contents.swift
+++ file_management.playground/Contents.swift
@@ -1,3 +1,93 @@
 import Cocoa
 
-var str = "Hello, playground"
+let fileManager = FileManager.default
+var filePath = "/Users/Shared/MyFolder/testFile.txt"
+
+//Create Folder
+var folderName = "/Users/Shared/MyFolder"
+do
+{
+    try fileManager.createDirectory(atPath : folderName, withIntermediateDirectories: true, attributes: nil)
+    
+    if(fileManager.isReadableFile(atPath: filePath))
+    {
+        print("The file is readable and can be written into")
+        let fileText = "This is the rudimentary initial file data from xcode"
+        let fileData = fileText.data(using:.utf8)
+        fileManager.createFile(atPath: filePath, contents: fileData, attributes:nil)
+    }
+    else
+    {
+        print("NOt readable")
+    }
+    if(fileManager.isWritableFile(atPath: filePath))
+    {
+        print("Writable")
+    }
+    else
+    {
+        print("Not writable")
+    }
+    if(fileManager.isDeletableFile(atPath: filePath))
+    {
+        print("Deletable")
+    }
+    else
+    {
+        print("Not deletable")
+    }
+    if(fileManager.isExecutableFile(atPath: filePath))
+    {
+        print("Executable")
+    }
+    else
+    {
+        print("Not executable")
+    }
+}
+catch let error
+{
+    print("Error : ", error.localizedDescription)
+}
+
+//Reading the file
+
+do
+{
+    let content = try String(contentsOfFile: filePath,encoding:.utf8)
+    print(content)
+}
+catch let error
+{
+    print("\(error.localizedDescription)")
+}
+
+//OverWrite Commands
+
+do
+{
+    let newText = "\n\n This is the changed block \n\n Written by : Jishant Acharya"
+    try newText.write(toFile: filePath, atomically: true, encoding: .utf8)
+    let content = try String(contentsOfFile: filePath,encoding:.utf8)
+    print(content)
+}
+catch let error
+{
+    print("\(error.localizedDescription)")
+}
+
+//Truncate the Contents
+
+let fileHandler = FileHandle(forWritingAtPath: filePath) // Handles the contents
+fileHandler?.truncateFile(atOffset: 0)
+
+//File delete
+
+/*do
+{
+    try fileManager.removeItem(atPath: filePath)
+}
+catch let error
+{
+    print("\(error.localizedDescription)")
+}*/
