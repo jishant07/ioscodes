2bc7f3491d16cc7e1c8514e5c44c048318485abc
admin <admin@ACE20.local>
2019-03-16 12:04:38 +0530

    Adding the image view App

---
+++ ImageDemo/ImageDemo/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ImageDemo
+//
+//  Created by admin on 16/03/19.
+//  Copyright © 2019 ACE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    var i = 1;
+    var name = " "
+    @IBOutlet weak var myImage: UIImageView!
+    @IBAction func changeImage(_ sender: UIButton)
+    {
+        i = i + 1;
+        if(i%6 == 0)
+        {
+            name = "img1";
+            i = 1;
+        }
+        else
+        {
+            name = "img\(i)";
+        }
+        myImage.image = UIImage(named:name);
+    }
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
+
