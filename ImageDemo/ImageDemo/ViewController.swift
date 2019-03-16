55eec3ce61b74cede4b16d18ae196a8f36c7304a
admin <admin@ACE20.local>
2019-03-16 13:02:08 +0530

    Swipe and Pinch Added

--- ImageDemo/ImageDemo/ViewController.swift
+++ ImageDemo/ImageDemo/ViewController.swift
@@ -11,9 +11,8 @@ import UIKit
 class ViewController: UIViewController
 {
     var i = 1;
-    var name = " "
-    @IBOutlet weak var myImage: UIImageView!
-    @IBAction func Tap(_ sender: UITapGestureRecognizer)
+    var name = " ";
+    func changeImageAhead()
     {
         i = i + 1;
         if(i%6 == 0)
@@ -26,6 +25,39 @@ class ViewController: UIViewController
             name = "img\(i)";
         }
         myImage.image = UIImage(named:name);
+    }
+    func changeImageBehind()
+    {
+        i = i - 1;
+        if(i%6 == 0)
+        {
+            name = "img1";
+            i = 1;
+        }
+        else
+        {
+            name = "img\(i)";
+        }
+        myImage.image = UIImage(named:name);
+    }
+    @IBOutlet weak var myImage: UIImageView!
+    @IBAction func Tap(_ sender: UITapGestureRecognizer)
+    {
+        changeImageAhead();
+    }
+    @IBAction func Swipe(_ sender: UISwipeGestureRecognizer)
+    {
+        changeImageBehind();
+    }
+    @IBAction func swipeLeft(_ sender: UISwipeGestureRecognizer)
+    {
+        changeImageAhead();
+    }
+    @IBAction func Pinch(_ sender: UIPinchGestureRecognizer)
+    {
+        //scale(x,y)
+        sender.view!.transform = sender.view!.transform.scaledBy(x: sender.scale , y: sender.scale);
+        sender.scale = 1;
     }
     override func viewDidLoad()
     {
