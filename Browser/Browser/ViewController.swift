08638d748cb172409d923635c2446b1f0b9ce3e6
admin <admin@ACE20.local>
2019-03-22 15:30:46 +0530

    Web Browser

---
+++ Browser/Browser/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Browser
+//
+//  Created by admin on 22/03/19.
+//  Copyright © 2019 ACE. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var urlText: UITextField!
+    @IBOutlet weak var webPage: WKWebView!
+    @IBOutlet weak var activityCheck: UIActivityIndicatorView!
+    func loadWeb(url:URL?)
+    {
+        var request = URLRequest(url : URL(string:"https://www.google.com")!)
+        if(url != nil)
+        {
+            request = URLRequest(url:url!)
+        }
+        webPage.load(request)
+    }
+    @IBAction func goButton(_ sender: UIButton)
+    {
+        let typed = "https://www."+urlText.text!
+        loadWeb(url: URL(string: typed))
+    }
+    @IBAction func goaheadButton(_ sender: UIButton)
+    {
+        webPage.goForward()
+    }
+    @IBAction func reloadButton(_ sender: UIButton)
+    {
+        webPage.reload()
+    }
+    @IBAction func gobackButton(_ sender: UIButton)
+    {
+        webPage.goBack()
+    }
+    @IBAction func exitButton(_ sender: UIButton)
+    {
+        exit(0)
+    }
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        let request = URLRequest(url : URL(string:"https://www.google.com")!)
+        webPage.load(request)
+    }
+}
+
