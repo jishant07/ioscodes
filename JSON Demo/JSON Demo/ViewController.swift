c3a137e5cb4d5f27b2bb84a8dc038431771b4bf8
jishant07 <jishanta@gmail.com>
2019-03-24 11:08:11 +0530

    JSON File Reader

---
+++ JSON Demo/JSON Demo/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  JSON Demo
+//
+//  Created by admin on 24/03/19.
+//  Copyright © 2019 ACE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func loadJSON(_ sender: UIButton)
+    {
+        let url = URL(string: "https://cognitioworld.com/dev/demo.json")
+        //let url = URL(string: "http://www.omdbapi.com/?s=fast+and+the+furious&apikey=4c95480a")
+        let dataTask = URLSession.shared.dataTask(with: url!)
+        { (data, response, err) in
+            if let jsonData = data
+            {
+                do
+                {
+                    let jsonContent = try JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers) as! [Any]
+                    let selectedArray = jsonContent[5] as! [String:Any]
+                    let dept = selectedArray["department"] as! [String:Any]
+                    let hr = dept["hr"] as! [Any]
+                    let emp = hr[1] as! [String:Any]
+                    print(emp["name"]!)
+                }
+                catch let err
+                {
+                    print(err)
+                }
+            }
+        }
+        dataTask.resume()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
